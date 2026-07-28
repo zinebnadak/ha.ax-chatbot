ebb19a0d3702fa2efb1af101fcc4f5264d4b7d27
zizo <zineb@nadak.ai>
2026-07-28 18:47:23 +0300

    feat: wp_scraper.py crawl4ai (built on playwright) for webscraping

--- backend/app/scraper/wp_scraper.py
+++ backend/app/scraper/wp_scraper.py
@@ -1,4 +1,18 @@
-# input 
-# output 
-# what type/shape is the output 
+# Send an HTTP request -> Receive the HTML/JSON response -> Parse the DOM -> Select elements -> Store structured data as json in /data
+# uv run playwright install chromium
+# input?
+# output?
+# type/shape of output?
 
+import asyncio
+from crawl4ai import *
+
+async def main():
+    async with AsyncWebCrawler() as crawler:
+        result = await crawler.arun(
+            url="https://www.ha.ax",
+        )
+        print(result.markdown)
+
+if __name__ == "__main__":
+    asyncio.run(main())
