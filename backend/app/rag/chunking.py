b65bfd420e9692f978dabb69894c377d4ce7538c
zizo <zineb@nadak.ai>
2026-08-09 11:11:13 +0300

    wip: chunking.py splitting the text into sections where ## headings

--- backend/app/rag/chunking.py
+++ backend/app/rag/chunking.py
@@ -5,6 +5,19 @@ def load_page(file_path: Path) -> dict:
     with file_path.open("r", encoding="utf-8") as file:
         return json.load(file) 
 
+def split_into_sections(text: str) -> list[str]:
+    sections = text.split("## ")
+    return sections
 
+
+# Testing
 page_path = Path("/Users/zizo/ha.ax-chatbot/data/bibliotek/en/about-the-library.json")
-print(load_page(page_path))
+page = load_page(page_path)
+
+title = page["title"]
+text = page["text"]
+sections = split_into_sections(text)
+for section in sections:
+    print("SECTION:")
+    print(section)
+    print()
