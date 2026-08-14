5605998f3349c5f4bd638e3d2db97f47a1b085f9
zizo <zineb@nadak.ai>
2026-08-14 09:19:22 +0300

    wip: embed_texts function all api keys added to .env, all SDKs installed, embed_texts function will be like a model dispatcher routing to 4 different models depending on what the script user calls.

--- backend/ha_rag_package/scripts/compare_embeddings.py
+++ backend/ha_rag_package/scripts/compare_embeddings.py
@@ -7,6 +7,11 @@ Flow:
 3. print all models answers and compare retrieval quality, or compare each models top-k (k *number of chunks most similar to the query) OR just use a similarity threshold
 
 Models I will be comparing:
+- `text-embedding-3-small`
+- `text-embedding-3-large
+- google gemini embeddings 001
+- cohere embed v4 
+- voyage multilingual -2`
 
 '''
 
