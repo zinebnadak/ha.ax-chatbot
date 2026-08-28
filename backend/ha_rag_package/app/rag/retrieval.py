2305b143a33532dfff2f04508ba1cd3a9778d5cc
zizo <zineb@nadak.ai>
2026-08-28 14:10:00 +0300

    test: tokenize the real documents data

--- backend/ha_rag_package/app/rag/retrieval.py
+++ backend/ha_rag_package/app/rag/retrieval.py
@@ -14,5 +14,21 @@ DATABASE_FOLDER = PROJECT_ROOT / "backend" / "chroma_db"
 result = collection.get(include=["documents", "metadatas"])
 print(len(result["ids"]))  # number of chunks in the collection
 
+def tokenize(text):
+    return re.findall(r"\w+", text.lower()) # fixes punktuations 
 
+tokenized_corpus = [tokenize(sentence) for sentence in result["documents"]]
+print(tokenized_corpus)
+
+'''
+query = "When is the office open?"
+tokenized_query = tokenize(query)
+print()
+print(tokenized_query)
+
+bm25 = BM25Okapi(tokenized_corpus)
+scores = bm25.get_scores(tokenized_query)
+print()
+print(scores)
+'''
 
